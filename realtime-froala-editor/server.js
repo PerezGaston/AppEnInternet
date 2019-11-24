7b8bc9eb02e91fdb1a977c5d84ec48aec3338942
janeko23 <munozjeanette23@gmail.com>
2019-11-23 22:00:40 -0300

    cambios en froala editor

--- realtime-froala-editor/server.js
+++ realtime-froala-editor/server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var socket = require('socket.io');
 var app = express();
-var server = app.listen( process.env.PORT || 8080);
+var server = app.listen( process.env.PORT || 3000);
 app.use(express.static('public'));
-console.log('Server listening on port 8080');
+console.log('Server listening on port 3000');
 var io = socket(server);
 
 io.sockets.on('connection', connection);
